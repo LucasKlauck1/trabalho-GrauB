4c8df15523644cf2f4bc9a9ac77a1fad56c2cf39
HelamD <helampathway@gmail.com>
2026-06-04 14:48:30 -0300

    locadora atributos

--- Locadora.java
+++ Locadora.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 
 public class Locadora {
-    
+    private ArrayList<Veiculo> veiculos;
+    private ArrayList<Locacao> locacoes;
+
+
+    public Locadora() {
+        this.veiculos = new ArrayList<>();
+        this.locacoes = new ArrayList<>();
+    }
 }
