28f8dc37c3c319a0b7f4a807ea154aa5f2846428
HelamD <helampathway@gmail.com>
2026-06-04 15:40:55 -0300

    Locadora: carregarDados e consultarVeiculo

--- Locadora.java
+++ Locadora.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Locadora {
     private ArrayList<Veiculo> veiculos;
@@ -9,4 +12,50 @@ public class Locadora {
         this.veiculos = new ArrayList<>();
         this.locacoes = new ArrayList<>();
     }
-}
+
+
+    public void carregarDados() {
+        String caminho = "veiculos.txt";
+
+        try( BufferedReader buffer =  new BufferedReader(new FileReader(caminho))) {
+            String linha;
+
+            while ((linha = buffer.readLine())!= null ) {
+                String[] partes = linha.split(";");
+
+                int codigo = Integer.parseInt(partes[0]);
+                String modelo = partes[1];
+                String cor = partes[2];
+                int ano = Integer.parseInt(partes[3]);
+                String cidade = partes[5];
+                boolean disponivel = Boolean.parseBoolean(partes[6]);
+                double valorDiaria = Double.parseDouble(partes[7]);
+                double valorKm =  Double.parseDouble(partes[8]);
+
+                
+                Veiculo v = new Veiculo(codigo, modelo, cor, ano, ano, cidade, disponivel, valorKm, valorKm);
+
+                this.veiculos.add(v);
+            }
+        } catch (IOException e) {
+            System.out.println("Error ao carregar dados: " + e.getMessage());
+        }
+    }
+
+    public Veiculo consultarVeiculo(int codigo) {
+        for (Veiculo v : veiculos) {
+            if (v.getCodigo() == codigo) {
+                return v;
+            }
+        }
+        return null;
+    }
+
+
+    }
+
+
+    
+
+
+
